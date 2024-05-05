1931678a8262f6b21f3c8048e3a82355dca4a4d3
Elysia <elysia.doan@avepoint.com>
2024-05-05 19:57:46 +0700

    asp

---
+++ ASP/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services.Implement;
+using Services.Interfaces;
+
+namespace ASP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpGet("getAll")]
+        public async Task<IActionResult> GetAllUsersAsync(int role, int method)
+        {
+            return Ok(await _userService.GetAllUsersAsync(role, method));
+        }
+    }
+}
---
+++ ASP/Program.cs
@@ -0,0 +1,32 @@
+using Repositories.Implement;
+using Repositories.Interfaces;
+using Services.Implement;
+using Services.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserService,UserService>();
+builder.Services.AddScoped<IUserRepository,UserRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
